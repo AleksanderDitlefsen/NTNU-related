0d5170034065e3490138b0cc132d20a955835d33
Aleksander Ditlefsen <ditlefsenaleksander@gmail.com>
2026-01-20 10:48:48 +0100

    Added statisticks program

---
+++ eksempeloppgave.py
@@ -0,0 +1,14 @@
+import random
+import numpy as np
+
+def TerningKast(n):
+    a_sum = 0
+    b_sum = 0
+    for i in range(n):
+        a = random.randint(1, 6)
+        b = random.randint(1, 6)
+        a_sum += a
+        b_sum += b
+    print(f'Average sum when throwing two die {n} times: {(a_sum + b_sum) / n}')
+
+TerningKast(1000000)
