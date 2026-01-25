5bc0e2dd43a14a0043f3c42d83120142a41a44f9
Aleksander Ditlefsen <ditlefsenaleksander@gmail.com>
2026-01-25 20:21:55 +0100

    Ferdig kode for andre innlevering i stat

--- Statistikk/stat_innlevering_2.py
+++ Statistikk/stat_innlevering_2.py
@@ -32,6 +32,8 @@ def simX(n):
             x_sim[i] = x[5]
     return x_sim
 
+
+"""
 n = 1000000
 x_simulert = simX(n)
 a = 0
@@ -41,3 +43,68 @@ for i in range(len(x_simulert)):
         a += 1
 
 print(" Sannsynligheten for at X er mindre enn eller lik 2 er ", a/n)
+"""
+
+"""
+n = 200_000
+x_simulert = simX(n)
+E = sum(x_simulert)/n
+print(f'Tilnermet verdi for E(x) med {n} simuleringer: ', E)
+
+
+
+Var = (sum(x_simulert**2) - n*E**2)/(n-1)
+print(f'Standartdavviket SD(x) er med {n} simuleringer tilnermet til: ', np.sqrt(Var))
+
+print(np.std(x_simulert, ddof=1))
+"""
+"""
+def simX2(n, alpha):
+    results = []
+    for i in range(n):
+        u = np.random.uniform()
+        results.append(np.sqrt(-np.log(1-u)*alpha))
+    return np.array(results)
+
+def f(x, alpha):
+    return (2*x/alpha)*np.exp(-x**2/alpha)
+    
+x_verdier = np.linspace(0, 4, 500)
+
+plt.hist(simX2(100000, 1), bins=100, density=True, alpha=0.6, color='g')
+plt.plot(x_verdier, f(x_verdier, 1), color='r')
+plt.show()
+"""
+
+
+def simY(n):
+    res = np.zeros(n)
+    
+    for i in range(n):
+        U = np.zeros(5)
+        
+        for j in range(5):
+            u = np.random.uniform()
+            U[j] = u
+        
+        a = np.array([np.sqrt(-np.log(1-U[0])),np.sqrt(-np.log(1-U[1])), 0, 0, 0])
+        
+        for k in range(2, 5):
+            b = np.sqrt(-np.log(1-U[k])*1.2)
+            a[k] = b
+            
+        a = np.sort(a)
+        res[i] = a[2]
+    return res
+
+y = simY(10_000)
+
+plt.hist(y, bins=100, density=True, alpha=0.6, color='k')
+plt.show()
+
+P_hat = np.mean(y >= 1)
+print(f'P(Y >= 1) er omtrent {P_hat}')
+print(f'P(Y >= 1 | Y > 0.75) er omtrent {(P_hat/np.mean(y > 0.75)):.5f}')
+
+print(f'Forventningsverdien til Y er omtrent {np.mean(y):.5f}')
+print(f'Standardavviket er omtrent {np.std(y, ddof=1):.5f}')
